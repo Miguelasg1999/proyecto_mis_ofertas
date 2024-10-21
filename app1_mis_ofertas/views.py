77733f65418e5da5d6345148bd021b0069c21aaf
Miguelasg1999 <miguelasg1999@gmail.com>
2024-10-21 09:50:43 -0300

    edit

--- app1_mis_ofertas/views.py
+++ app1_mis_ofertas/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 #from .forms import 
 
 #Colores hexadecimales para el proyecto
@@ -15,7 +16,40 @@ def home(request):
     return render(request, 'index.html')
 
 def signin(request):
-    return render(request, 'signin.html')
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Usuario o contraseña incorrectos'
+            })
+        else:
+            login(request, user)
+            return redirect('home')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'El usuario ya existe'
+                })
+        return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Las Contraseñas no coinciden'
+                })
